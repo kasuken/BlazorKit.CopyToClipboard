58924aa08a55582f5d71cbe772a7133cfa368b23
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2023-04-04 15:22:58 +0200

    first release + sample

---
+++ src/BlazorKit.CopyToClipboard/IClipboardService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorKit.CopyToClipboard
+{
+    public interface IClipboardService
+    {
+        Task CopyToClipboard(string text);
+    }
+
+    public class ClipboardService : IClipboardService
+    {
+        private readonly IJSRuntime _jsInterop;
+
+        public ClipboardService(IJSRuntime jsInterop)
+        {
+            _jsInterop = jsInterop;
+        }
+
+        public async Task CopyToClipboard(string text)
+        {
+            await _jsInterop.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
